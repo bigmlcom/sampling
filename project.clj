bbb4ca39eecd4439e59dcb0d39bb98244d6d03d6
Adam Ashenfelter <ashenfad@gmail.com>
2013-01-01 14:34:56 -0800

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sample "2.0.0"
+(defproject sample "2.0.1"
   :description "Random Sampling in Clojure"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [incanter/parallelcolt "0.9.4"]
